2f1c0d63aaa28cf900185e5a489dc05b3180122e
jhethia <jhethia@gmail.com>
2024-04-24 10:28:25 +0530

    Added Project Structure And Created Controller File

---
+++ AuthService/src/main/java/com/prakarshs/DhanBuddy/AuthService/Controller/DhanBuddyAuthController.java
@@ -0,0 +1,9 @@
+package com.prakarshs.DhanBuddy.AuthService.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dhanbuddy/auth")
+public class DhanBuddyAuthController {
+}
